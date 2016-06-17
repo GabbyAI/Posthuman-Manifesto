89733c773bce5c2d80be5841e857851309bd41fa
Gabae <madej@riseup.net>
2016-06-17 07:24:57 -0400

    Add files via upload

---
+++ cron.bak.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+MODEL_REPO=~/gabbynill_model
+for sourcename in $(grep ORIGINAL .env | grep -v ADMIN |  cut -d"=" -f2)
+do
+	ebooks archive $sourcename
+	ebooks consume corpus/${sourcename}.json
+done
+cp model/*model $MODEL_REPO/
+pushd $MODEL_REPO
+date=$(date)
+git commit -m "sync at $date" .
+git push origin master
+popd
+heroku ps:restart 
+
